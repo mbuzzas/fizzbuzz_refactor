fc245516a84037ea3f716d976da88f3b0e757c65
mbuzzas <mbuzzas3@gmail.com>
2016-09-06 18:51:01 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+var limit = +prompt("Enter a number between 5 and 100")
+parseInt(limit)
+
+function fizzbuzz() {
+	for (var i = 1; i <= limit; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			$('body').append('<p>fizz buzz</p>');
+		}
+		else if (i % 3 === 0) {
+			$('body').append('<p>fizz</p>');
+		}
+		else if (i % 5 === 0) {
+			$('body').append('<p>buzz</p>');
+		}
+		else {
+			$('body').append('<p>' + i + '</p>');
+		}
+	}
+}
+
+fizzbuzz();
+	
+})
